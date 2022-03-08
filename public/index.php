c4efc19bee0b7996780824cc9261b749b3c698b7
Youness Mouhajir <marialalij@gmail.com>
2022-03-08 23:01:57 +0100

    document technique

--- public/index.php
+++ public/index.php
@@ -1,16 +1,20 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-    Debug::enable();
-}
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);

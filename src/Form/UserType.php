534686bca1f39fc6675832f9a9647e377d76e609
Youness Mouhajir <marialalij@gmail.com>
2022-03-10 01:54:55 +0100

    final modif

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -17,15 +17,31 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add('username', TextType::class, [
+                'label' => "Nom d'utilisateur",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux mots de passe doivent correspondre.',
                 'required' =>  true,
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
+                'first_options' => ['label' => 'Mot de passe',  "attr" => [
+                    "class" => "form-control"
+                ]],
+                'second_options' => ['label' => 'Tapez le mot de passe à nouveau',  "attr" => [
+                    "class" => "form-control"
+                ]],
+
             ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
             ->add('roles', ChoiceType::class, [
                 'choices' => [
                     'Utilisateur' => 'ROLE_USER',
@@ -35,6 +51,10 @@ class UserType extends AbstractType
                 'expanded' => false,
                 'label' => 'Rôle',
                 'required' => true,
+                "attr" => [
+                    "class" => "form-control"
+                ]
+
             ]);
     }
 }
